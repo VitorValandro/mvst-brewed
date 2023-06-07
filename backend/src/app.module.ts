f03a1b76fae43ed45953b5010da7037a5c927839
Vitor Valandro <vitormateusd@gmail.com>
2023-06-06 21:19:53 -0300

    chore: config typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CodeChallengeController } from './code-challenge/code-challenge.controller';
-import { CoffeeEntity } from './code-challenge/code-challenge.entity';
-import { CodeChallengeModule } from './code-challenge/code-challenge.module';
+import { CodeChallengeController } from './modules/code-challenge/code-challenge.controller';
+import { CodeChallengeModule } from './modules/code-challenge/code-challenge.module';
+import dataSource, { typeormDatabaseConfiguration } from './db/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'mvst-coffee-tea-challenge-db',
-      entities: [CoffeeEntity],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(typeormDatabaseConfiguration()),
     CodeChallengeModule,
   ],
   controllers: [CodeChallengeController],
   providers: [],
 })
 export class AppModule {}
+
+dataSource.initialize();
---
+++ backend/src/db/typeorm.config.ts
@@ -0,0 +1,31 @@
+import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { config } from 'dotenv';
+
+config();
+
+export const typeormDatabaseConfiguration = (): PostgresConnectionOptions => {
+  const config: PostgresConnectionOptions = {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    migrationsTableName: 'migrations',
+    migrations: [__dirname + '/src/db/migrations/*{.ts,.js}'],
+    synchronize: true,
+  };
+
+  if (process.env.NODE_ENV !== 'local') {
+    return {
+      ...config,
+      synchronize: false,
+    };
+  }
+
+  return config;
+};
+
+export default new DataSource(typeormDatabaseConfiguration());
