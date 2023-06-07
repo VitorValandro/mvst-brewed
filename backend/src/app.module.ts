f02329b16100e9d25e6f1cfb91f31f60072dbfe1
Vitor Valandro <vitormateusd@gmail.com>
2023-06-07 06:53:04 -0300

    chore: setup static images bucket

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { CoffeeModule } from './modules/coffee/coffee.module';
 import dataSource, { typeormDatabaseConfiguration } from './db/typeorm.config';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'bucket'),
+      serveRoot: join('/bucket'),
+    }),
     TypeOrmModule.forRoot(typeormDatabaseConfiguration()),
     CoffeeModule,
   ],
