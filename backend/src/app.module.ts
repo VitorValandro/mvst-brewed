f8262c8b71caa760de0e0b40416ebfc43939170d
Vitor Valandro <vitormateusd@gmail.com>
2023-06-07 21:30:06 -0300

    fix: serve static files through express

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,10 @@
-import { join, resolve } from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { CoffeeModule } from './modules/coffee/coffee.module';
 import dataSource, { typeormDatabaseConfiguration } from '../typeorm.config';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot(
-      (() => {
-        const bucketDir = resolve('./bucket');
-        return {
-          rootPath: bucketDir,
-          serveRoot: join('/bucket'),
-        };
-      })(),
-    ),
     TypeOrmModule.forRoot(typeormDatabaseConfiguration()),
     CoffeeModule,
   ],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use('/bucket', express.static(resolve('./mock-bucket')));
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
